294d631e0d56bb5326d853d22fd2436e74c2d64f
Jad Bizri <91910175+JadBizri@users.noreply.github.com>
2024-10-20 20:16:34 -0400

    Completed recent projects section

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import {ThemeProvider} from "next-themes";
+import React from "react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
