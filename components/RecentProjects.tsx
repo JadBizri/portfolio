b69173a80a2078b53998abb10f8df3759efef4d8
Jad Bizri <91910175+JadBizri@users.noreply.github.com>
2024-10-23 14:52:39 -0400

    Optimized css class

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -49,7 +49,7 @@ const RecentProjects = () => {
                                     as={Link}
                                     href={link}
                                     target="_blank"
-                                    className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                                    className="px-4 py-2 rounded-xl text-xs font-normal text-white"
                                 >
                                     Try now →
                                 </CardItem>
